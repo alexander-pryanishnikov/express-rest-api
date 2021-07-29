340e1eb92aff9a036dcb20c49f9d73d0bd54f4f3
apryanishnikov <ap@zaoblako.ru>
2021-07-29 14:29:18 +0300

    tech: Поправил index.ts AP-2

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const user = require('./routes/user');
+const PORT = process.env.PORT || 3000
 
 app.use(user);
 
-app.listen(3000);
+app.listen(3000, () => {
+	console.log(`Server is running on PORT: ${PORT}..`)
+});

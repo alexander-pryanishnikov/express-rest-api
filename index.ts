ca4c78c4ce9bb44fd323d36979a388e4f3aaf7ca
apryanishnikov <ap@zaoblako.ru>
2021-08-02 18:58:07 +0300

    feat: - добавил JWT AP-2

--- index.ts
+++ index.ts
@@ -1,9 +1,19 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const user = require('./routes/user');
+const user = require('./routes/user.route');
 const PORT = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+
+const response_time = require('./middleware/response-time.middleware')
+
+
+app.use(response_time)
+
+app.use(bodyParser.json())
 
 app.use(user);
+
+
 
 app.listen(PORT, () => {
 	console.log(`Server is running on PORT: ${PORT}..`)
